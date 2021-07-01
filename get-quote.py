84c54ec3b5c7e58bb023c7117fafebd164e13e41
Burnt0asty <jake.w.miranda@outlook.com>
2021-07-02 00:15:21 +0530

    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,13 @@
+import random
 def toast():
   print("Keep it logically awesome lol.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  last = 13
+  rnd = random.randint(0, last)
+  print(quotes[rnd])
 
 if __name__== "__main__":
   toast()
